d98094e845b2e89b8e5ed72976064de64ee9ec3d
wojtaz31 <wojtaz31@wp.pl>
2024-04-05 14:41:23 +0200

    loadUserPhotos now loads all .png and .jpg files regardless name

--- script_utils.py
+++ script_utils.py
@@ -3,14 +3,15 @@ import os
 
 def loadUserPhotos(folder_path):
   photos = []
-  for i in range(len(os.listdir(folder_path))):
-    path = f'{folder_path}probka{i+1}.png'
-    photos.append(path)
+  for filename in os.listdir(folder_path):
+    if filename.lower().endswith(('.png', '.jpg')):
+      path = f'/content/drive/MyDrive/user_photos_data/{i}'
+      photos.append(path)
   return photos
 
 def loadTiles(folder_path):
   tileMap = []
-  for i in range(len(os.listdir(folder_path))):
+  for i in range(len(os.listdir(folder_path))-1):
     path = f'{folder_path}{i+1}.png'
     photo = cv2.imread(path,cv2.IMREAD_COLOR)
     tileMap.append([path, photo, 0])
