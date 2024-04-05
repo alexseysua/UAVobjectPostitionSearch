4f8f04eb9561fa64e4c30c615bca9b21b9287947
wojtaz31 <wojtaz31@wp.pl>
2024-04-05 12:57:16 +0200

    amount of map tiles isn't hard coded, script reads amount of files in data source folder

--- script_utils.py
+++ script_utils.py
@@ -1,8 +1,9 @@
 import cv2
+import os
 
 def loadTiles(folder_path):
   tileMap = []
-  for i in range(32):
+  for i in range(len(os.listdir(folder_path))):
     path = f'{folder_path}{i+1}.png'
     photo = cv2.imread(path,cv2.IMREAD_COLOR)
     tileMap.append([path, photo, 0])
