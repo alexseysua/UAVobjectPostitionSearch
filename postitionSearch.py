4613e382a408163ef3543604ce282b497b95b7c1
wojtaz31 <wojtaz31@wp.pl>
2024-04-05 12:09:27 +0200

    added back functional loop

--- postitionSearch.py
+++ postitionSearch.py
@@ -6,5 +6,7 @@ drive.mount('/content/drive')
 
 #paths to files with photos with object you want to determine location
 userPhotos = ['/content/probka3.png', '/content/probka4.png']
-
 tileMap = loadTiles('/content/drive/MyDrive/dane/')
+
+for path in userPhotos:
+  printFormattedMatches(findMatches(path, tileMap))
